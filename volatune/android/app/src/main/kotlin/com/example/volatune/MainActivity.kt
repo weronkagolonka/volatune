ae7c052da3550e986d5f26e0f69d02093cf86b32
Weronka Golonka <54840864+weronkagolonka@users.noreply.github.com>
2025-06-25 19:49:01 +0200

    refactor: remove flutter project

--- volatune/android/app/src/main/kotlin/com/example/volatune/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.volatune
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- volatune/lib/common/set_status.dart
+++
@@ -1,6 +0,0 @@
-import 'package:logger/logger.dart';
-
-void setStatus(String code, Logger logger, {String? message}) {
-  var text = message ?? '';
-  logger.i('$code$text');
-}
