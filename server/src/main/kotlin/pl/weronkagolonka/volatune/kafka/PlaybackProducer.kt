66534f5d70cfd100b4e5d7bbe46fa22b1689f616
Weronka Golonka <54840864+weronkagolonka@users.noreply.github.com>
2025-07-27 22:33:37 +0200

    refactor: update producer to send Playback records
    - use custom serializer

--- server/src/main/kotlin/pl/weronkagolonka/volatune/kafka/PlaybackProducer.kt
+++ server/src/main/kotlin/pl/weronkagolonka/volatune/kafka/PlaybackProducer.kt
@@ -1,11 +1,6 @@
 package pl.weronkagolonka.volatune.kafka
 
 import com.github.avrokotlin.avro4k.Avro
-import com.github.avrokotlin.avro4k.encodeToByteArray
-import com.github.avrokotlin.avro4k.schema
-import io.confluent.kafka.serializers.KafkaAvroSerializer
-import kotlinx.serialization.encodeToByteArray
-import org.apache.avro.Schema
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -13,34 +8,33 @@ import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.serialization.StringSerializer
 import pl.weronkagolonka.volatune.KafkaConfiguration
 import pl.weronkagolonka.volatune.domain.events.Playback
+import pl.weronkagolonka.volatune.domain.events.PlaybackKafkaSerializer
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 class PlaybackProducer(
-    config: KafkaConfiguration,
+    private val config: KafkaConfiguration,
     private val avroInstance: Avro,
-    private val topic: String,
-    private val schema: Schema,
 ) {
     private val producer =
-        KafkaProducer<String, ByteArray>(
-            buildMap {
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to config.bootstrapServers
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java.name
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to KafkaAvroSerializer::class.java.name
-                "schema.registry.url" to config.schema.url
-                "auto.register.schemas" to config.schema.autoRegister
-                "use.latest.version" to config.schema.useLatestVersion
-            },
+        KafkaProducer<String, Playback>(
+            mapOf(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to config.bootstrapServers,
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java.canonicalName,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to PlaybackKafkaSerializer::class.java.canonicalName,
+                "schema.registry.url" to config.schema.url,
+                "auto.register.schemas" to config.schema.autoRegister,
+                "use.latest.version" to config.schema.useLatestVersion,
+            ),
         )
 
     suspend fun send(playback: Playback) {
         val record =
             ProducerRecord(
-                topic,
+                config.topic,
                 playback.location,
-                avroInstance.encodeToByteArray<Playback>(playback),
+                playback,
             )
 
         suspendCoroutine<RecordMetadata> { continuation ->
