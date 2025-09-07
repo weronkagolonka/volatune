57a1775c0881b34d9670cdbb4ca5189c95eb54c4
Weronka Golonka <54840864+weronkagolonka@users.noreply.github.com>
2025-09-07 19:25:09 +0200

    refactor: remove kafkaCleaner listener
    - don't remove the topic but rather restart the cluster between tests

--- common/src/main/kotlin/pl/weronka/golonka/volatune/common/test/Listeners.kt
+++ common/src/main/kotlin/pl/weronka/golonka/volatune/common/test/Listeners.kt
@@ -2,52 +2,14 @@ package pl.weronka.golonka.volatune.common.test
 
 import io.kotest.core.listeners.TestListener
 import io.kotest.core.spec.Spec
-import io.kotest.core.test.TestCase
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonPrimitive
 import org.apache.avro.Schema
-import org.apache.kafka.clients.admin.AdminClient
-import org.apache.kafka.clients.admin.AdminClientConfig
 import org.junit.jupiter.api.fail
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
-import kotlin.collections.filterNot
-import kotlin.collections.map
-
-class KafkaCleanerListener(
-    val bootstrapServers: String,
-) : TestListener {
-    private lateinit var admin: AdminClient
-
-    override suspend fun beforeSpec(spec: Spec) {
-        admin =
-            AdminClient.create(
-                mapOf(
-                    AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
-                ),
-            )
-    }
-
-    override suspend fun beforeTest(testCase: TestCase) {
-        admin.listTopics().names().get().filterNot { it.startsWith("_") }.let {
-            if (it.isNotEmpty()) {
-                admin.deleteTopics(it).all().get()
-            }
-        }
-
-        admin.listConsumerGroups().all().get().map { it.groupId() }.let {
-            if (it.isNotEmpty()) {
-                admin.deleteConsumerGroups(it).all().get()
-            }
-        }
-    }
-
-    override suspend fun afterSpec(spec: Spec) {
-        admin.close()
-    }
-}
 
 class SchemaRegistererListener(
     val registryUrl: String,
